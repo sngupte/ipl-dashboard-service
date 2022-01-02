d73bb4ccaffa422ddd04f7d55e5a91f06341689e
sngupte <56316465+sngupte@users.noreply.github.com>
2022-01-02 19:50:29 +0530

    Matchpage Created

--- src/frontend/src/Pages/MatchPage.js
+++ src/frontend/src/Pages/MatchPage.js
@@ -1,11 +1,28 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { MatchDetailCard } from '../Components/MatchDetailsCard';
+import { MatchSmallCard } from '../Components/MatchSmallCard';
+
 
 export const MatchPage = () => {
-
-    
+    const [matches, setMatches] = useState([]);
+    const {teamName, year} = useParams();
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+            console.log(data);
+        }
+        fetchMatches();
+    }, []
+    );
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
+            {
+                matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
+            }
         </div>
     );
 }
