8c118808ae3e04e5e9b3bc4f90a8b1a5f1e70156
sngupte <56316465+sngupte@users.noreply.github.com>
2022-01-02 19:48:59 +0530

    Matches By Year API Created

--- src/main/java/io/ipl/ipldashboard/controller/TeamController.java
+++ src/main/java/io/ipl/ipldashboard/controller/TeamController.java
@@ -1,13 +1,16 @@
 package io.ipl.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.ipl.ipldashboard.model.Match;
 import io.ipl.ipldashboard.model.Team;
 import io.ipl.ipldashboard.repository.MatchRepository;
 import io.ipl.ipldashboard.repository.TeamRepository;
@@ -21,14 +24,21 @@ public class TeamController {
 	@Autowired
 	private MatchRepository matchRepository;
 
-	
-
 	@GetMapping("/team/{teamName}")
 	public Team getTeam(@PathVariable String teamName) {
 		System.out.println(teamName);
 		Team team = teamRepository.findByTeamName(teamName);
 		team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
 		return team;
-	} 
+	}
+
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+		List<Match> matchList = matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+		System.out.println(matchList);
+		return matchList;
+	}
 
 }
